ef0877101e7a04912eea06c205ca74134392c5a3
Dennis Amparo <dennisamparo11@gmail.com>
2022-02-08 16:06:05 -0700

    js, working on update logic

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
+// tag to append list
 const list = $("#listhere");
 
-const addTodo = () => {
+// function that creates to dos
+const addToDo = () => {
   if ($("#todos").val() != "") {
     let aToDo = $("#todos").val().trim();
-    list.append($("<li>").append($("<input>").val(aToDo).prop("disabled", true)).append($("<button>").text("modify")).append($("<button>").text("save")));
+    list.append(
+      $("<li>")
+        .css("padding-bottom", "15px")
+        .append(
+          $("<input>").val(aToDo).prop("disabled", true).addClass(aToDo)
+        )
+        .append(
+          $("<button>")
+            .text("update")
+            .addClass("update")
+            .attr("data-update", aToDo)
+        )
+        .append(
+          $("<button>")
+            .text("save")
+            .addClass("save")
+            .addClass("save")
+            .attr("data-save", aToDo)
+        )
+    );
+
     $("#todos").val("");
   }
 };
 
+// this function allows the enables the enter key for the input field
 $("#todos").keypress(function (event) {
   if (event.which == 13) {
-    addTodo();
+    addToDo();
   }
 });
 
+// runs the addToDo function via the add button
 $("#addbtn").on("click", function () {
-  addTodo();
+  addToDo();
 });
+
+// create function that targets either the modify or save btn
+$("#listhere").on("click", ".update", function (event) {
+  event.preventDefault();
+  let updateInput = $(this).attr("data-update");
+  let forClass = '"' + "." + updateInput + '"';
+  console.log(forClass);
+
+
+
+
+//   if ( updateInput)
+//   console.log(updateInput);
+//   let justTheValue = ""+ updateInput + "";
+//   console.log(justTheValue);
+  $(forClass).prop("disabled", false);
+});
+
+// $("#listhere").on("click", ".save", function (event) {
+//   event.preventDefault();
+//   $(".modifyEntry").prop("disabled", true);
+// });
