d14c9efb1d89efaf88e26f5dd6510f7a65590ec7
Dennis Amparo <dennisamparo11@gmail.com>
2022-02-22 18:40:44 +0100

    router not working

---
+++ controllers/twentydays.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+
+const todo = require('../models/twentydays');
+
+
+
+
+// serve main page using this url endpoint
+router.get("/", (req, res)=>
+{
+//    res.sendFile(path.join(__dirname, "index.html"));
+   res.render("index");
+
+ });
+ 
+router.get("/twentydays", (req, res)=>
+ {
+    // res.sendFile(path.join(__dirname, "index.html"));
+    res.render("index");
+
+  });
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require('path');
-const PORT = 8080;
+// const path = require('path');
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 // middleware to parse json requests
@@ -10,33 +10,13 @@ app.use(express.json());
 app.use("/public", express.static("public"));
 
 
-// serve main page using this url endpoint
-app.get("/", (req, res)=>
-{
-   res.sendFile(path.join(__dirname, "index.html"));
 
- });
- 
-app.get("/twentydays", (req, res)=>
- {
-    res.sendFile(path.join(__dirname, "index.html"));
-
-  });
 
  
 // POST route build
+const router = require('./controllers/twentydays');
 
-// app.get('/starwars', (req, res)=>{
-
-//     res.status(200).send({
-
-//         starwars: 'Captain Rex',
-//         jedi: 'ahosoka tano',
-//         formerSith: 'darth maul'
-
-//     })
-
-// });
+app.use(router);
 
 
 
